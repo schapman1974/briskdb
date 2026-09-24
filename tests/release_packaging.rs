0bea3e87a0753713321a78abd17301004995934d
Stephen Chapman <schapman1974@gmail.com>
2026-09-24 13:56:53 -0400

    fix: align the separate fuzz lockfile with alpha.7

--- tests/release_packaging.rs
+++ tests/release_packaging.rs
@@ -1,6 +1,10 @@
 #[test]
 fn alpha_release_contract_covers_every_native_archive_and_safety_boundary() {
     assert_eq!(env!("CARGO_PKG_VERSION"), "0.1.0-alpha.7");
+    assert!(include_str!("../fuzz/Cargo.lock").contains(&format!(
+        "name = \"briskdb\"\nversion = {:?}",
+        env!("CARGO_PKG_VERSION")
+    )));
 
     let workflow = include_str!("../.github/workflows/release.yml");
     for required in [
